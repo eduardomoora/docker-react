6c6255fc2d1465697baf8b19e176610d4f9faa0d
Eddie <ecemo10101@gmail.com>
2023-05-20 01:45:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hey hi there
+          hey hi there from the feature branch
         </p>
         <a
           className="App-link"
